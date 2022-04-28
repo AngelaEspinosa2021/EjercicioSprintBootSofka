cf400eec3f2a7736517a34e0122d0d10dffac2e5
AngelaEspinosa <titagena@gmail.com>
2022-04-28 02:36:37 -0500

    CRUD tabla Usuario y CRUD tabla UsuarioROl

--- sprintboot/src/main/java/com/trainingsofka/sprintboot/services/UserRolService.java
+++ sprintboot/src/main/java/com/trainingsofka/sprintboot/services/UserRolService.java
@@ -1,2 +1,53 @@
-package com.trainingsofka.sprintboot.services;public class UserRolService {
+package com.trainingsofka.sprintboot.services;
+
+import com.trainingsofka.sprintboot.models.UserModel;
+import com.trainingsofka.sprintboot.models.UserRolModel;
+import com.trainingsofka.sprintboot.repositories.UserRepository;
+import com.trainingsofka.sprintboot.repositories.UserRolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class UserRolService {
+    @Autowired
+    UserRolRepository userRolRepository;
+
+    public ArrayList<UserRolModel> obtenerRoles(){
+        return (ArrayList<UserRolModel>) userRolRepository.findAll();
+    }
+
+    public UserRolModel guardarRol(UserRolModel rol){
+        return userRolRepository.save(rol);
+    }
+
+    public Optional<UserRolModel> obtenerPorIdRol(Integer id_rol){
+        return userRolRepository.findById(id_rol);
+    }
+
+    public Optional<UserRolModel> obtenerPorIdUsuario(Integer id_usuario){
+        return userRolRepository.findById(id_usuario);
+    }
+
+    public boolean eliminarRol(Integer id_rol) {
+        try{
+            userRolRepository.deleteById(id_rol);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
+
+    public boolean eliminarRolesAsociadosalIdUsuario(Integer id_usuario) {
+        try{
+            userRolRepository.deleteById(id_usuario);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
+
+
 }
--- sprintboot/src/main/java/com/trainingsofka/sprintboot/services/UserService.java
+++ sprintboot/src/main/java/com/trainingsofka/sprintboot/services/UserService.java
@@ -1,4 +1,40 @@
 package com.trainingsofka.sprintboot.services;
 
+import com.trainingsofka.sprintboot.models.UserModel;
+import com.trainingsofka.sprintboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public ArrayList<UserModel> obtenerUsuarios(){
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    public UserModel guardarUsuario(UserModel usuario){
+        return userRepository.save(usuario);
+    }
+
+    public Optional<UserModel> obtenerPorIdUsuario(Integer id_usuario){
+        return userRepository.findById(id_usuario);
+    }
+
+    public ArrayList<UserModel>  obtenerPorPrioridad(Integer prioridad) {
+        return userRepository.findByPrioridad(prioridad);
+    }
+
+    public boolean eliminarUsuario(Integer id_usuario) {
+        try{
+            userRepository.deleteById(id_usuario);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
 }
